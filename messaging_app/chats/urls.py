4ff368a420997edbcbe3ad653786207a8789d159
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-07-26 17:21:31 +0100

    Ensure chats/urls.py uses DefaultRouter without NestedDefaultRouter

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,10 +2,15 @@
 
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
 router.register(r'messages', MessageViewSet)
 
-urlpatterns = router.urls
+# This is NestedDefaultRouter - you DON'T want this for the current checker
+nested_router = nested_routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+# nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
+urlpatterns = router.urls + nested_router.urls
