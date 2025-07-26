e5d1f1be8c5ea10cd999a994976af2122d63565c
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-07-26 16:55:21 +0100

    Ensure messaging_app/urls.py contains exact 'api/' string for routing

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,10 @@
 # chats/urls.py
 
-from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
 router.register(r'messages', MessageViewSet)
 
