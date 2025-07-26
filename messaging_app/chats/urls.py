9bb62e88c2560793e811d7d9d39cb20cb9ef4a10
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-07-26 17:44:45 +0100

    Complete Task 5: Successfully run and test the messaging application with all endpoints working

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,15 +2,10 @@
 
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework_nested import routers as nested_routers
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
 router.register(r'messages', MessageViewSet)
 
-# This is NestedDefaultRouter - you DON'T want this for the current checker
-nested_router = nested_routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
-# nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
-
-urlpatterns = router.urls + nested_router.urls
+urlpatterns = router.urls
