0dad284e9362a868619b20a5bbbbc8cec84988b7
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-07-26 11:05:33 +0100

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Unit tests for utils.access_nested_map and utils.get_json.
+Unit tests for utils.access_nested_map, utils.get_json, and utils.memoize.
 """
 
 import unittest
@@ -8,7 +8,7 @@ from typing import Any, Mapping, Tuple
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -64,6 +64,26 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests for the memoize decorator."""
+
+    def test_memoize(self) -> None:
+        """Test that a_method is called only once even if a_property is accessed twice."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+            self.assertEqual(obj.a_property, 42)
+            self.assertEqual(obj.a_property, 42)
+            mock_method.assert_called_once()
 
 
 if __name__ == '__main__':
