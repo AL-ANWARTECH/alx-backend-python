1f3ea628aba1de83d8236bd6b62483759a633a6c
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-07-27 17:13:54 +0100

    Cirrection

---
+++ Django-Middleware-0x03/settings.py
@@ -0,0 +1,16 @@
+# Django-Middleware-0x03/settings.py
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Custom Middlewares
+    'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
+    'chats.middleware.OffensiveLanguageMiddleware',
+]
